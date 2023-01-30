45cbe22d6673ad89433d051f926d412fab08e78f
Saphira Kai <kai.saphira@gmail.com>
2023-01-30 09:46:22 -0400

    add script for cleaning repos

---
+++ clean-repos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#? safer bash options
+set -euo pipefail
+
+function clean() {
+    pushd "$1" >/dev/null
+    cargo clean &>/dev/null
+    popd >/dev/null
+    echo "cleaned $1"
+}
+
+pushd repos/ >/dev/null
+for repo in *; do
+    clean $repo &
+done
+wait
+popd >/dev/null
