74251ac89694237ed9e5b0983812dc515e4224b1
Saphira Kai <kai.saphira@gmail.com>
2023-03-11 22:26:31 -0400

    add verbose debug logging (why didn't i think of this sooner)

--- util.sh
+++ util.sh
@@ -1,9 +1,12 @@
 [ -d repos/ ] || ./setup.sh
 [ -f .hmd-setup ] || ./hmd-setup.sh
+[ -f /tmp/telescope.log ] && rm /tmp/telescope.log
 
 #? build the named repo
 function build() {
+    echo "[util.sh] build - building: $@" >> /tmp/telescope.log
     for repo in $@; do
+        echo "[util.sh] build - built: $repo" >> /tmp/telescope.log
         pushd "repos/$repo" >/dev/null
         2>&1 >/dev/null cargo build --release
         popd >/dev/null
@@ -12,6 +15,7 @@ function build() {
 
 #? run the named repo
 function run() {
+    echo "[util.sh] run - $1" >> /tmp/telescope.log
     pushd "repos/$1" >/dev/null
     shift
     2>&1 >/dev/null cargo run --release $@ &
@@ -24,6 +28,8 @@ for i in {0..32}; do
     ! [ -f "$lockfile" ] || flock -w 0.01 "$lockfile" true && {
         export WAYLAND_DISPLAY="wayland-$i"
         set_display_success=true
+        echo "[util.sh] - WAYLAND_DISPLAY=$WAYLAND_DISPLAY" >> /tmp/telescope.log
+        echo >> /tmp/telescope.log
         break
     }
 done
@@ -32,6 +38,8 @@ ${set_display_success:-false} || echo "warning: failed to set WAYLAND_DISPLAY pr
 #? generate variables holding the absolute path to each repo's executable
 pushd repos >/dev/null
 for repo in *; do
+    echo "[util.sh] setup - $repo" >> /tmp/telescope.log
+    echo "[util.sh]       - target_dir=$PWD/$repo/target/release/" >> /tmp/telescope.log
     target_dir="$PWD/$repo/target/release/"
  
     [ -d "$target_dir" ] || {
@@ -42,11 +50,17 @@ for repo in *; do
     
     pushd "$target_dir" >/dev/null
     for file in *; do
-        [ -f "$file" ] && [ -x "$file" ] && target_exe="$file" && break
+        [ -f "$file" ] && [ -x "$file" ] && {
+            echo "[util.sh]       - target_exe=$file" >> /tmp/telescope.log
+            target_exe="$file"
+        } && break
     done
     popd >/dev/null
 
+    echo "[util.sh] - ${repo}_exe=$target_dir/$target_exe" >> /tmp/telescope.log
     eval "${repo}_exe=$target_dir/$target_exe"
+    
+    echo >> /tmp/telescope.log
 done
 popd >/dev/null
 
